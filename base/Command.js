31f2ccb75d01da85234bc6083e463f5042b888b3
DanPlayz0 <damuffin36@gmail.com>
2021-06-24 08:53:23 +0000

    Added error catching

--- base/Command.js
+++ base/Command.js
@@ -1,21 +1,33 @@
 class Command {
 
-  constructor (client, {
-    name = null,
-    description = "No description provided.",
-    category = "Uncategorized",
-    usage = "No usage provided.",
-    enabled = true,
-    guildOnly = false,
-    aliases = [],
-    permLevel = "User",
-    cooldown = 1,
-    grabable = true,
-    botOwner = false,
-  }) {
+  constructor (client, options) {
     this.client = client;
-    this.conf = { enabled, guildOnly, aliases, permLevel, cooldown, grabable, botOwner };
-    this.help = { name, description, category, usage };
+    this.conf = {
+      enabled: options.enabled || true,
+      guildOnly: options.guildOnly || false,
+      aliases: options.aliases || [],
+      permLevel: options.permLevel || "User",
+      cooldown: (options.cooldown || options.cooldown == 0) ? options.cooldown : 1,
+      options: options.options || [],
+    };
+    this.help = { 
+      name: options.name || 'None',
+      description: options.description || "No description provided.",
+      category: options.category || "Uncategorized",
+      usage: (options.usage || options.usage == "") ? options.usage : "No usage provided."
+    };
+  }
+
+  run() {
+    throw new Error(`${this.constructor.name} doesn't have a run() method.`)
+  }
+
+  async _run(ctx) {
+    try {
+      await this.run(ctx);
+    } catch (err) {
+      ctx.client.logger.log(err.message, 'error');
+    }
   }
 }
 module.exports = Command;
